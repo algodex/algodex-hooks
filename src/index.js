a1c1039c5529ea2b413a2bf474aaac2f51a7addf
Michael J Feher <github@phearzero.com>
2022-03-29 22:19:24 -0500

    📦 Export Provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ export {
   default as useAlgodex,
 } from './useAlgodex.js';
 
-export {default as AlgodexContext} from './components/AlgodexContext.js';
+export {
+  default as AlgodexContext,
+  Provider,
+} from './components/AlgodexContext.js';
 export {
   default as useAssetOrderbookQuery,
 } from './http/useAssetOrderbookQuery.js';
