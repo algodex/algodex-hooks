b453e8747828de72cc464049dd51dffb19e8f0c0
Michael J Feher <github@phearzero.com>
2022-04-05 20:13:44 -0500

    🚚 Move open orders mapping

--- src/http/useWalletOrdersQuery.js
+++ src/http/useWalletOrdersQuery.js
@@ -1,7 +1,5 @@
 import ServiceError from '../components/ServiceError';
 import Spinner from '../components/Spinner';
-import dayjs from 'dayjs';
-import floatToFixed from '@algodex/algodex-sdk/lib/utils/format/floatToFixed';
 import useAlgodex from '../useAlgodex.js';
 import {useMemo} from 'react';
 import {useQuery} from 'react-query';
@@ -26,68 +24,7 @@ export function withWalletOrdersQuery(Component, options) {
     ...options,
   });
 }
-/**
- *
- * @param {object} data
- * @return {null|*[]}
- */
-export function mapOpenOrdersData(data) {
-  if (
-    !data ||
-    !data.buyASAOrdersInEscrow ||
-    !data.sellASAOrdersInEscrow ||
-    !data.allAssets
-  ) {
-    return null;
-  }
 
-  const {
-    buyASAOrdersInEscrow: buyOrdersData,
-    sellASAOrdersInEscrow: sellOrdersData,
-    allAssets: assetsData,
-  } = data;
-
-  const assetsInfo = assetsData.reduce((allAssetsInfo, currentAssetInfo) => {
-    allAssetsInfo[currentAssetInfo.index] = currentAssetInfo;
-    return allAssetsInfo;
-  }, {});
-
-  const buyOrders = buyOrdersData.map((order) => {
-    const {assetId, formattedPrice, formattedASAAmount, unix_time} = order;
-    return {
-      asset: {id: assetId},
-      date: dayjs.unix(unix_time).format('YYYY-MM-DD HH:mm:ss'),
-      // date: moment(unix_time, 'YYYY-MM-DD HH:mm').format(),
-      unix_time: unix_time,
-      price: floatToFixed(formattedPrice),
-      pair: `${assetsInfo[assetId].params['unit-name']}/ALGO`,
-      type: 'BUY',
-      status: 'OPEN',
-      amount: formattedASAAmount,
-      metadata: order,
-    };
-  });
-
-  const sellOrders = sellOrdersData.map((order) => {
-    const {assetId, formattedPrice, formattedASAAmount, unix_time} = order;
-
-    return {
-      asset: {id: assetId},
-      date: dayjs.unix(unix_time).format('YYYY-MM-DD HH:mm:ss'),
-      unix_time: unix_time,
-      price: floatToFixed(formattedPrice),
-      pair: `${assetsInfo[assetId].params['unit-name']}/ALGO`,
-      type: 'SELL',
-      status: 'OPEN',
-      amount: formattedASAAmount,
-      metadata: order,
-    };
-  });
-
-  const allOrders = [...buyOrders, ...sellOrders];
-  allOrders.sort((a, b) => (a.unix_time < b.unix_time ? 1 : -1));
-  return allOrders;
-}
 /**
  * Use Wallet Orders Query
  *
@@ -107,6 +44,6 @@ export default function useWalletOrdersQuery({
       () => http.dexd.fetchWalletOrders(address),
       options,
   );
-  const orders = useMemo(() => mapOpenOrdersData(data), [data]);
+  const orders = useMemo(() => http.dexd.mapOpenOrdersData(data), [data]);
   return {data: {orders}, ...rest};
 }
