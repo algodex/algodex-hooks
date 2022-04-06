e2e969f3cda9d31b6b6a2b78c886f9436d70aad6
Ikechi <kechieleazar@gmail.com>
2022-04-06 23:21:50 +0100

    ✅ Update test for asset price query

---
+++ src/http/useAssetPriceQuery.spec.js
@@ -0,0 +1,29 @@
+import nock from 'nock';
+import {renderHook} from '@testing-library/react-hooks';
+import useAssetPriceQuery from './useAssetPriceQuery.js';
+import {wrapper} from '../../test/setup.js';
+
+describe('Fetch Trade History', () => {
+  it('should fetch asset trade history', async () => {
+    // jest.setTimeout(60000);
+    const asset = {
+      id: 69410904,
+    };
+    if (process.env.TEST_ENV !== 'integration') {
+      nock('https://testnet.algodex.com/algodex-backend')
+          .get(`/trade_history.php?assetId=${asset.id}`)
+          .reply(200, require('./__tests__/fetchAssetTradeHistory.json'));
+    }
+    const {result, waitFor} = renderHook(
+        () => useAssetPriceQuery({asset}),
+        {wrapper},
+    );
+    await waitFor(() => {
+      return result.current.isSuccess;
+    });
+
+    expect(result.current.isError).toBe(false);
+    expect(result.current.isLoading).toBe(false);
+    expect(Object.keys(result.current.data)).toEqual(['orders']);
+  });
+});
