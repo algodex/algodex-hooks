642a3c43464724f17eb33c2969bd636f26b515a9
Michael J Feher <github@phearzero.com>
2022-04-06 16:03:41 -0500

    ✅ Update API mock to work with integration testing

--- src/useAlgodex.js
+++ src/useAlgodex.js
@@ -1,7 +1,9 @@
 import {useContext} from 'react';
 import AlgodexContext from './components/AlgodexContext.js';
+import fetchWallets from '@algodex/algodex-sdk/lib/wallet/http/fetchWallets';
 // import useMyAlgo from './wallet/useMyAlgo.js';
 import useWatch from './utils/useWatch.js';
+import {useQuery} from 'react-query';
 
 /**
  * @typedef {Object} AlgodexAPIHook
@@ -35,20 +37,17 @@ export default function useAlgodex() {
     typeof algodex.addresses !== 'undefined' &&
     algodex.addresses.length > 0;
 
-  // On MyAlgo Changes, update the AlgodexAPI
-  // const handleMyAlgoChange = useCallback(
-  //     (response) => {
-  //       console.log(response);
-  //       if (response == null) {
-  //         return;
-  //       }
-  //       algodex.setAddresses(response);
-  //     },
-  //     [algodex],
-  // );
 
-  // Hook into MyAlgo
-  // const connect = useMyAlgo({onChange: handleMyAlgoChange});
+  // TODO move to constructed instance of HTTPClient
+  // TODO move to a proper useWalletsQuery
+  const {data, isLoading, isError} = useQuery(
+      'wallets',
+      () => fetchWallets(algodex.config, algodex.addresses),
+      {
+        enabled: isConnected,
+        refetchInterval: 3000,
+      },
+  );
 
   // Return algodex and Connect
   return {
@@ -64,7 +63,7 @@ export default function useAlgodex() {
     setAsset: (...args)=>algodex.setAsset(...args),
     config: algodex.config,
     setConfig: (...args)=>algodex.setConfig(...args),
-    addresses: algodex.addresses,
+    addresses: isLoading || isError ? algodex.addresses : data,
     setAddresses: (...args)=>algodex.setAddresses(...args),
     placeOrder: (...args)=>algodex.placeOrder(...args),
   };
