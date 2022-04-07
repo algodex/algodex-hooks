5f8b4c9d9e2a4844287cc9a322ab7216dc65cb07
Ikechi <kechieleazar@gmail.com>
2022-04-07 14:58:54 +0100

    ✅ Complete testing for useExplorer Asset and Price WQuery

--- src/http/useAlgoExplorer.spec.js
+++ src/http/useAlgoExplorer.spec.js
@@ -15,32 +15,7 @@ describe('Fetch Explorer Asset', () => {
     if (process.env.TEST_ENV !== 'integration') {
       nock('https://testnet.algoexplorerapi.io')
           .get(`/v1/asset/${asset.id}/info`)
-          .reply(200, {
-            'txid': 'VAU74RXRJQ5V3KX4YKSNAMUSOPS2BSZXGWOX5EMLXLLW4MC65YMA',
-            'timestamp': 1644253464,
-            'decimals': 10,
-            'creator':
-                'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-            'owner': null,
-            'assetID': 69410904,
-            'assetName': 'Zero',
-            'unitName': 'ZERO',
-            'url': 'https://phearzero.com',
-            'defaultFrozen': false,
-            'clawbackAccount':
-                'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-            'freezeAccount':
-                'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-            'reserveAccount':
-                'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-            'managerAccount':
-                'TJFFNUYWHPPIYDE4DGGYPGHWKGAPJEWP3DGE5THZS3B2M2XIAPQ2WY3X4I',
-            'totalSupply': 13370000000000,
-            'txCount': 31,
-            'circulatingSupply': 30012195344,
-            'verified': false,
-            'destroyed': false,
-          });
+          .reply(200, require('./__tests__/fetchExplorerAssetInfo.json'));
     }
     const {result, waitFor} = renderHook(
         () => useExplorerAssetInfo({asset}),
@@ -49,14 +24,19 @@ describe('Fetch Explorer Asset', () => {
     await waitFor(() => {
       return result.current.isSuccess;
     });
-
     expect(result.current.isError).toBe(false);
     expect(result.current.isLoading).toBe(false);
     expect(result.current.isSuccess).toBe(true);
-    // expect(Object.keys(result.current.data)).toEqual([
-    //   'sellASAOrdersInEscrow',
-    //   'buyASAOrdersInEscrow',
-    // ]);
+    expect(Object.keys(result.current.data)).toEqual(
+        [
+          'id', 'deleted',
+          'txid', 'timestamp',
+          'decimals', 'name',
+          'txns', 'fullName',
+          'circulating', 'verified',
+          'url', 'total',
+        ],
+    );
   });
 });
 
@@ -65,16 +45,7 @@ describe('Fetch Algorand Price Query', () => {
     if (process.env.TEST_ENV !== 'integration') {
       nock(`https://price.algoexplorerapi.io`)
           .get(`/price/algo-usd`)
-          .reply(200, {
-            'coin': 'ALGO-USD',
-            'timestamp': 1649336804,
-            'price': '0.7864',
-            'best_bid': '0.7862',
-            'best_ask': '0.7864',
-            'open_24h': '0.7816',
-            'low_24h': '0.7509',
-            'high_24h': '0.8032',
-          });
+          .reply(200, require('./__tests__/fetchAlgorandPrice.json'));
     }
     const {result, waitFor} = renderHook(
         () => useAlgorandPriceQuery(),
@@ -87,9 +58,7 @@ describe('Fetch Algorand Price Query', () => {
     expect(result.current.isError).toBe(false);
     expect(result.current.isLoading).toBe(false);
     expect(result.current.isSuccess).toBe(true);
-    // expect(Object.keys(result.current.data)).toEqual([
-    //   'sellASAOrdersInEscrow',
-    //   'buyASAOrdersInEscrow',
-    // ]);
+    expect(Object.keys(result.current.data)).toEqual(['algoPrice']);
+    expect(typeof result.current.data.algoPrice).toBe('string');
   });
 });
