9f75df16fd47f7da51750f08f72ed27d7b938c9a
Michael J Feher <github@phearzero.com>
2022-04-11 22:11:42 -0500

    ✅ Test setter validation in hooks

--- src/useAlgodex.spec.js
+++ src/useAlgodex.spec.js
@@ -1,3 +1,4 @@
+import ValidationError from '@algodex/algodex-sdk/lib/Errors/ValidationError';
 import {renderHook} from '@testing-library/react-hooks';
 import useAlgodex from './useAlgodex.js';
 import {wrapper} from '../test/setup.js';
@@ -17,5 +18,13 @@ describe('useAlgodex Hook', () => {
       'setConfig', 'addresses',
       'setAddresses', 'placeOrder',
     ]);
+
+    expect(()=>result.current.setWallet({})).toThrowError(ValidationError);
+    expect(()=>result.current.setAsset({})).toThrowError(ValidationError);
+    expect(()=>result.current.setAddresses({})).toThrowError(ValidationError);
+    result.current.setOrder({type: 'sell'});
+    expect(result.current.algodex.order.type).toEqual('sell');
+    expect(()=>result.current.setConfig({})).toThrowError(ValidationError);
+    // expect(()=>result.current.placeOrder()).toThrowError(ValidationError);
   });
 });
