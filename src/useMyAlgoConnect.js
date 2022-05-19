d1f84e53f6a664dedc6813a420d8b94fff943121
Michael J Feher <github@phearzero.com>
2022-05-18 20:56:08 -0500

    🚨 Lint Fixes

--- src/useMyAlgoConnect.js
+++ src/useMyAlgoConnect.js
@@ -1,68 +1,78 @@
-import { useEffect, useRef } from 'react'
-import algosdk from 'algosdk'
-import { assignGroups, groupBy } from '@algodex/algodex-sdk/lib/functions/base'
+import {useEffect, useRef} from 'react';
+import algosdk from 'algosdk';
+import {assignGroups, groupBy} from '@algodex/algodex-sdk/lib/functions/base';
 
 const ERROR = {
   FAILED_TO_INIT: 'MyAlgo Wallet failed to initialize.',
-  FAILED_TO_CONNECT: 'MyAlgo Wallet failed to connect.'
-}
+  FAILED_TO_CONNECT: 'MyAlgo Wallet failed to connect.',
+};
 
 /**
  * useMyAlgoConnect
+ * @param {Function} onConnect On Connect Callback
+ * @param {Function} onDisconnect On Disconnect Callback
  * @return {WalletEffect}
  */
 export default function useMyAlgoConnect(onConnect, onDisconnect) {
   // Instance reference
-  const myAlgoWallet = useRef()
+  const myAlgoWallet = useRef();
   /**
    * MyAlgoConnect Signer
    *
    * @todo move to SDK
-   * @param outerTxns
+   * @param {Object} outerTxns
    * @return {Promise<*>}
    */
   async function signer(outerTxns) {
-    console.debug('inside signMyAlgoTransactions transactions')
-    const groups = groupBy(outerTxns, 'groupNum')
+    console.debug('inside signMyAlgoTransactions transactions');
+    const groups = groupBy(outerTxns, 'groupNum');
 
-    const numberOfGroups = Object.keys(groups)
+    const numberOfGroups = Object.keys(groups);
 
     const groupedGroups = numberOfGroups.map((group) => {
       const allTxFormatted = groups[group].map((txn) => {
-        return txn.unsignedTxn
-      })
-      assignGroups(allTxFormatted)
-      return allTxFormatted
-    })
+        return txn.unsignedTxn;
+      });
+      assignGroups(allTxFormatted);
+      return allTxFormatted;
+    });
 
-    const flattenedGroups = groupedGroups.flat()
+    const flattenedGroups = groupedGroups.flat();
 
-    const txnsForSig = []
+    const txnsForSig = [];
+
+    /**
+     * Is UserSigned Transaction
+     * @param {Object} outerTxn
+     * @return {boolean}
+     */
     function isUserSigned(outerTxn) {
-      return typeof outerTxn.lsig === 'undefined' && typeof outerTxn.senderAcct !== 'undefined'
+      return typeof outerTxn.lsig === 'undefined' &&
+        typeof outerTxn.senderAcct !== 'undefined';
     }
     for (let i = 0; i < outerTxns.length; i++) {
-      outerTxns[i].unsignedTxn = flattenedGroups[i]
+      outerTxns[i].unsignedTxn = flattenedGroups[i];
       if (isUserSigned(outerTxns[i])) {
-        txnsForSig.push(flattenedGroups[i].toByte())
+        txnsForSig.push(flattenedGroups[i].toByte());
       }
     }
 
-    const signedTxnsFromUser = await this.signTransaction(txnsForSig)
+    // eslint-disable-next-line no-invalid-this
+    const signedTxnsFromUser = await this.signTransaction(txnsForSig);
 
     if (Array.isArray(signedTxnsFromUser)) {
-      let userSigIndex = 0
+      let userSigIndex = 0;
       for (let i = 0; i < outerTxns.length; i++) {
         if (isUserSigned(outerTxns[i])) {
-          outerTxns[i].signedTxn = signedTxnsFromUser[userSigIndex].blob
-          userSigIndex++
+          outerTxns[i].signedTxn = signedTxnsFromUser[userSigIndex].blob;
+          userSigIndex++;
         }
       }
     } else {
       for (let i = 0; i < outerTxns.length; i++) {
         if (isUserSigned(outerTxns[i])) {
-          outerTxns[i].signedTxn = signedTxnsFromUser.blob
-          break
+          outerTxns[i].signedTxn = signedTxnsFromUser.blob;
+          break;
         }
       }
     }
@@ -70,40 +80,40 @@ export default function useMyAlgoConnect(onConnect, onDisconnect) {
     for (let i = 0; i < outerTxns.length; i++) {
       if (!isUserSigned(outerTxns[i])) {
         const signedLsig = algosdk.signLogicSigTransactionObject(
-          outerTxns[i].unsignedTxn,
-          outerTxns[i].lsig
-        )
-        outerTxns[i].signedTxn = signedLsig.blob
+            outerTxns[i].unsignedTxn,
+            outerTxns[i].lsig,
+        );
+        outerTxns[i].signedTxn = signedLsig.blob;
       }
     }
-    return outerTxns.map((o) => o.signedTxn)
+    return outerTxns.map((o) => o.signedTxn);
   }
 
   const connect = async () => {
     try {
       // Something went wrong!
       if (!myAlgoWallet.current) {
-        console.error(ERROR.FAILED_TO_INIT)
-        return
+        console.error(ERROR.FAILED_TO_INIT);
+        return;
       }
 
       // Get Accounts from MyAlgo
-      const accounts = await myAlgoWallet.current.connect()
+      const accounts = await myAlgoWallet.current.connect();
 
       // Map the connector to the address list
       const _addresses = accounts.map((acct) => {
-        acct.type = 'my-algo-wallet'
-        acct.connector = myAlgoWallet.current
-        acct.connector.connected = true
-        return acct
-      })
-      console.debug('Setting Address form myAlgoConnect', _addresses)
+        acct.type = 'my-algo-wallet';
+        acct.connector = myAlgoWallet.current;
+        acct.connector.connected = true;
+        return acct;
+      });
+      console.debug('Setting Address form myAlgoConnect', _addresses);
       // Set Addresses
-      onConnect(_addresses)
+      onConnect(_addresses);
     } catch (e) {
-      console.error(ERROR.FAILED_TO_CONNECT, e)
+      console.error(ERROR.FAILED_TO_CONNECT, e);
     }
-  }
+  };
 
   const disconnect = () => {
     // setAddresses(
@@ -113,24 +123,24 @@ export default function useMyAlgoConnect(onConnect, onDisconnect) {
     // if (algodex.addresses.length) {
     //   setWallet(algodex.addresses[0], { validate: false, merge: true })
     // }
-  }
+  };
 
   useEffect(() => {
     const initMyAlgoWallet = async () => {
       // '@randlabs/myalgo-connect' is imported dynamically
       // because it uses the window object
-      const MyAlgoConnect = (await import('@randlabs/myalgo-connect')).default
-      MyAlgoConnect.prototype.sign = signer
+      const MyAlgoConnect = (await import('@randlabs/myalgo-connect')).default;
+      MyAlgoConnect.prototype.sign = signer;
       // TODO: get signer from SDK
       // MyAlgoConnect.prototype.sign = await import(
       //   '@algodex/algodex-sdk/lib/wallet/signers/MyAlgoConnect'
       // )
-      myAlgoWallet.current = new MyAlgoConnect()
-      myAlgoWallet.current.connected = false
-    }
+      myAlgoWallet.current = new MyAlgoConnect();
+      myAlgoWallet.current.connected = false;
+    };
 
-    initMyAlgoWallet()
-  }, [])
+    initMyAlgoWallet();
+  }, []);
 
-  return { connect, disconnect, onDisconnect, connector: myAlgoWallet.current }
+  return {connect, disconnect, onDisconnect, connector: myAlgoWallet.current};
 }
--- src/useWallets.js
+++ src/useWallets.js
@@ -1,9 +1,9 @@
-import { useAlgodex } from '@algodex/algodex-hooks'
-import useMyAlgoConnect from './useMyAlgoConnect'
-import useWalletConnect from './useWalletConnect'
-import { useCallback, useEffect, useState } from 'react'
-import { isEqual } from 'lodash/lang'
-import events from '@algodex/algodex-sdk/lib/events'
+import {useAlgodex} from '@algodex/algodex-hooks';
+import useMyAlgoConnect from './useMyAlgoConnect';
+import useWalletConnect from './useWalletConnect';
+import {useCallback, useEffect, useState} from 'react';
+import {isEqual} from 'lodash/lang';
+import events from '@algodex/algodex-sdk/lib/events';
 /**
  *
  * @param {Array<Wallet>} a
@@ -13,83 +13,93 @@ import events from '@algodex/algodex-sdk/lib/events'
  */
 function _mergeAddresses(a, b) {
   if (!Array.isArray(a) || !Array.isArray(b)) {
-    throw new TypeError('Must be an array of addresses!')
+    throw new TypeError('Must be an array of addresses!');
   }
-  const map = new Map()
-  a.forEach((wallet) => map.set(wallet.address, wallet))
-  b.forEach((wallet) => map.set(wallet.address, { ...map.get(wallet.address), ...wallet }))
-  return Array.from(map.values())
+  const map = new Map();
+  a.forEach((wallet) => map.set(wallet.address, wallet));
+  b.forEach((wallet) => map.set(wallet.address,
+      {...map.get(wallet.address), ...wallet},
+  ));
+  return Array.from(map.values());
 }
 
+/**
+ * Use Wallets Hooks
+ * @param {Object} initialState Wallet Initial State
+ * @return {*}
+ */
 function useWallets(initialState) {
-  const [wallet, setWallet] = useState(initialState)
-  const [activeWallet, setActiveWallet] = useState()
-  const [addresses, setAddresses] = useState([])
-  const { http } = useAlgodex()
+  const [wallet, setWallet] = useState(initialState);
+  const [activeWallet, setActiveWallet] = useState();
+  const [addresses, setAddresses] = useState([]);
+  const {http} = useAlgodex();
 
   const onEvents = useCallback(
-    (props) => {
-      const { type, wallet: _wallet } = props
-      if (type === 'change' && !isEqual(wallet, _wallet)) {
-        setWallet(_wallet)
-        setActiveWallet(_wallet.address)
-      }
-    },
-    [setWallet, wallet]
-  )
+      (props) => {
+        const {type, wallet: _wallet} = props;
+        if (type === 'change' && !isEqual(wallet, _wallet)) {
+          setWallet(_wallet);
+          setActiveWallet(_wallet.address);
+        }
+      },
+      [setWallet, wallet],
+  );
   useEffect(() => {
-    events.on('wallet', onEvents)
+    events.on('wallet', onEvents);
     return () => {
-      events.off('wallet', onEvents)
-    }
-  }, [onEvents])
+      events.off('wallet', onEvents);
+    };
+  }, [onEvents]);
 
   // TODO: Account Info Query
   // Handle any Connection
   const handleConnect = useCallback(
-    async (_addresses) => {
-      const accounts = await http.indexer.fetchAccounts(_addresses)
-      console.log(accounts)
-      setAddresses(_mergeAddresses(_addresses, accounts))
-    },
-    [setAddresses]
-  )
+      async (_addresses) => {
+        const accounts = await http.indexer.fetchAccounts(_addresses);
+        console.log(accounts);
+        setAddresses(_mergeAddresses(_addresses, accounts));
+      },
+      [setAddresses],
+  );
 
   // Handle any Disconnect
   const handleDisconnect = useCallback((_addresses) => {
-    console.error('Handle removing from storage', _addresses)
-  }, [])
+    console.error('Handle removing from storage', _addresses);
+  }, []);
 
   // My Algo Connect/Disconnect
-  const { connect: myAlgoConnect, disconnect: myAlgoDisconnect } = useMyAlgoConnect(
-    handleConnect,
-    handleDisconnect
-  )
+  const {
+    connect: myAlgoConnect,
+    disconnect: myAlgoDisconnect,
+  } = useMyAlgoConnect(
+      handleConnect,
+      handleDisconnect,
+  );
   // Pera Connect/Disconnect
-  const { connect: peraConnect, disconnect: peraDisconnect } = useWalletConnect(
-    handleConnect,
-    handleDisconnect
-  )
+  const {connect: peraConnect, disconnect: peraDisconnect} = useWalletConnect(
+      handleConnect,
+      handleDisconnect,
+  );
 
   // Fetch active wallet from local storage
   useEffect(() => {
-    const res = localStorage.getItem('activeWallet')
+    const res = localStorage.getItem('activeWallet');
     if (res && res !== activeWallet) {
-      setActiveWallet(localStorage.getItem('activeWallet'))
+      setActiveWallet(localStorage.getItem('activeWallet'));
     }
-  }, [setActiveWallet])
+  }, [setActiveWallet]);
 
   // Fetch all wallet addresses from local storage
   useEffect(() => {
-    const res = localStorage.getItem('addresses')
+    const res = localStorage.getItem('addresses');
     if (res) {
-      setAddresses(JSON.parse(localStorage.getItem('addresses')))
+      setAddresses(JSON.parse(localStorage.getItem('addresses')));
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('addresses', JSON.stringify(addresses))
-  }, [addresses])
+    localStorage.setItem('addresses', JSON.stringify(addresses));
+  }, [addresses]);
 
   return {
     wallet,
@@ -98,8 +108,8 @@ function useWallets(initialState) {
     myAlgoConnect,
     peraConnect,
     peraDisconnect,
-    myAlgoDisconnect
-  }
+    myAlgoDisconnect,
+  };
 }
 
-export default useWallets
+export default useWallets;
